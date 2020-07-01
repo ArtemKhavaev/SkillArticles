34e73099738f610b833d06f4415f4533d7288286
artem <artemkhavaev@gmail.com>
2020-07-01 16:44:51 +0300

    hometask_4

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -1,25 +1,24 @@
 package ru.skillbranch.skillarticles.extensions
 
 import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.marginBottom
-import androidx.core.view.marginLeft
-import androidx.core.view.marginRight
-import androidx.core.view.marginTop
 import androidx.core.widget.NestedScrollView
-import kotlinx.android.synthetic.main.activity_root.view.*
 import ru.skillbranch.skillarticles.R
 
+
+
 fun View.setMarginOptionally(bottom: Int) {
-    val scroll = findViewById<NestedScrollView>(R.id.scroll)
-//    scroll.bottom = bottom
-    scroll.setBottom(bottom)
-    this.setBottom(bottom)
+    ViewGroup.MarginLayoutParams((this as ViewGroup).layoutParams)
+        .setMargins(0, 0, 0, bottom)
 }
 
-fun  View.setMarginOptionally(left:Int = marginLeft, top : Int = marginTop, right : Int = marginRight, bottom : Int = marginBottom){
-    val scroll = findViewById<NestedScrollView>(R.id.scroll)
-    scroll.bottom = bottom
-    scroll.top = top
-    scroll.left = left
-    scroll.right = right
-}
+//fun  View.setMarginOptionally(left:Int = marginLeft, top : Int = marginTop, right : Int = marginRight, bottom : Int = marginBottom){
+//    val scroll = findViewById<NestedScrollView>(R.id.scroll)
+//    scroll.bottom = bottom
+//    scroll.top = top
+//    scroll.left = left
+//    scroll.right = right
+//}
