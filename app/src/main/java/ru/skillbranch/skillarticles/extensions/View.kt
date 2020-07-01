b83b531b7d60fceace7120853aca4c0d86f15ac7
artem <artemkhavaev@gmail.com>
2020-07-01 22:37:41 +0300

    hometask_4

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -11,8 +11,22 @@ import ru.skillbranch.skillarticles.R
 
 
 fun View.setMarginOptionally(bottom: Int) {
-    ViewGroup.MarginLayoutParams((this as ViewGroup).layoutParams)
-        .setMargins(0, 0, 0, bottom)
+
+//    val p : ViewGroup.LayoutParams = this.getLayoutParams()
+//    val scroll = findViewById<NestedScrollView>(R.id.scroll)
+//
+//    scroll.layoutParams= p as scroll.layoutParams;
+//        if (_default) lp.setMargins(mc.oml, mc.omt, mc.omr, mc.omb);
+//        else lp.setMargins(mc.ml, mc.mt, mc.mr, mc.mb);
+//    this.layoutParams = lp;
+
+    val params: ViewGroup.MarginLayoutParams = this.getLayoutParams() as ViewGroup.MarginLayoutParams
+//    params.bottomMargin = bottom
+    params.setMargins(0, 0, 0, bottom)
+
+
+//    ViewGroup.MarginLayoutParams((this as ViewGroup).layoutParams)
+//        .setMargins(0, 0, 0, bottom)
 }
 
 //fun  View.setMarginOptionally(left:Int = marginLeft, top : Int = marginTop, right : Int = marginRight, bottom : Int = marginBottom){
